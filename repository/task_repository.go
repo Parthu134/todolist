27223040bc825d4a66788c56401ddbbe8ad6a6dd
Parthu <bheemanathiparthu@example.com>
2025-09-29 11:33:03 +0530

    cron

--- repository/task_repository.go
+++ repository/task_repository.go
@@ -14,7 +14,6 @@ type TaskRepository interface {
 	UpdateRepo(task entities.Task) (entities.Task, error)
 	DeleteRepo(id uint) error
 	GetTaskDueBefore(deadline time.Time) ([]entities.Task, error)
-	DeleteTaskOlderThan(cutoff time.Time) (int64, error)
 }
 
 type taskRepository struct {
@@ -53,9 +52,4 @@ func (r *taskRepository) GetTaskDueBefore(deadline time.Time) ([]entities.Task, 
 	var tasks []entities.Task
 	err := r.db.Where("due_date <= ?", deadline).Find(&tasks).Error
 	return tasks, err
-}
-
-func (r *taskRepository) DeleteTaskOlderThan(cutoff time.Time) (int64, error) {
-	result := r.db.Where("due_date <= ?", cutoff).Delete(&entities.Task{})
-	return result.RowsAffected, result.Error
 }
--- repository/task_repository_backup.go
+++ repository/task_repository_backup.go
@@ -1,15 +1,12 @@
 package repository
 
 import (
-	"time"
 	"todo-list/entities"
 
 	"gorm.io/gorm"
 )
 
 type TaskBackupRepository interface{
-	DeleteOldBackups(cutoff time.Time) (int64, error)
-	GetAllBackups() ([]entities.TaskBackup, error)
 	CreateBackupRepo(backup entities.TaskBackup) (entities.TaskBackup, error)
 }
 type taskBackupRepository struct {
@@ -23,15 +20,4 @@ func NewTaskBackupRepository(db *gorm.DB) TaskBackupRepository {
 func (r *taskBackupRepository) CreateBackupRepo(backup entities.TaskBackup) (entities.TaskBackup, error) {
 	err := r.DB.Create(&backup).Error
 	return backup, err
-}
-
-func (r *taskBackupRepository) DeleteOldBackups(cutoff time.Time) (int64, error) {
-	result := r.DB.Where("backup_time <= ?", cutoff).Delete(&entities.TaskBackup{})
-	return result.RowsAffected, result.Error
-}
-
-func (r *taskBackupRepository) GetAllBackups() ([]entities.TaskBackup, error) {
-	var backups []entities.TaskBackup
-	err := r.DB.Find(&backups).Error
-	return backups, err
 }
